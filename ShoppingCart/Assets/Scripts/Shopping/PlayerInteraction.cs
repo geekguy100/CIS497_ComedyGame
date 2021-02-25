d455afc98c57baa250b632d27cc7e2d0dfce4f15
Kyle Grenier <kylegrenier@users.noreply.github.com>
2021-02-25 01:06:20 -0600

    New ground item interaction: Anything within ur radius is picked up. Need to incorporate Chris's system into this.

--- ShoppingCart/Assets/Scripts/Shopping/PlayerInteraction.cs
+++ ShoppingCart/Assets/Scripts/Shopping/PlayerInteraction.cs
@@ -6,26 +6,60 @@
 // Brief Description : Script that handles how the player interacts with IInteractables.
 *****************************************************************************/
 using UnityEngine;
+using System.Collections.Generic;
 
 [RequireComponent(typeof(CharacterInteraction))]
 public class PlayerInteraction : MonoBehaviour
 {
     CharacterInteraction characterInteraction;
+    List<IInteractable> interactablesNearby = new List<IInteractable>();
 
     private void Awake()
     {
         characterInteraction = GetComponent<CharacterInteraction>();
     }
 
-    // If the player is within the IInteractable
-    private void OnTriggerStay(Collider col)
+    //// If the player is within the IInteractable
+    //private void OnTriggerStay(Collider col)
+    //{
+    //    if (col.CompareTag("Interactable"))
+    //    {
+    //        IInteractable interactable = col.GetComponent<IInteractable>();
+    //        if (Input.GetKeyDown(KeyCode.E) && interactable != null)
+    //        {
+    //            characterInteraction.Interact(interactable);
+    //        }
+    //    }
+    //}
+
+    private void OnTriggerEnter(Collider col)
     {
-        if (col.CompareTag("Interactable"))
+        IInteractable i = col.GetComponent<IInteractable>();
+        if (i != null)
         {
-            IInteractable interactable = col.GetComponent<IInteractable>();
-            if (Input.GetKeyDown(KeyCode.E) && interactable != null)
+            if (!interactablesNearby.Contains(i))
+                interactablesNearby.Add(i);
+        }
+    }
+
+    private void OnTriggerExit(Collider col)
+    {
+        IInteractable i = col.GetComponent<IInteractable>();
+        if (i != null)
+        {
+            if (interactablesNearby.Contains(i))
+                interactablesNearby.Remove(i);
+        }
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            for (int i = 0; i < interactablesNearby.Count; ++i)
             {
-                characterInteraction.Interact(interactable);
+                characterInteraction.Interact(interactablesNearby[i]);
+                interactablesNearby.Remove(interactablesNearby[i]);
             }
         }
     }
