c1dadad32a601ff095b795298992560f4f416453
Kyle Grenier <kyle.grenier11@gmail.com>
2021-03-07 04:01:46 -0600

    Changed how NPCs get stunned so it's more consistent. Now checks if the player is mid-dash instead of checking the magnitude of the force applied to the cart.

--- ShoppingCart/Assets/Scripts/NPCCart.cs
+++ ShoppingCart/Assets/Scripts/NPCCart.cs
@@ -9,19 +9,15 @@ using UnityEngine;
 
 public class NPCCart : MonoBehaviour
 {
-    [Tooltip("The magnitude of incoming force needed to stun the NPC.")]
-    [SerializeField] private float stunMagnitude = 6f;
-
     private void OnCollisionEnter(Collision col)
     {
-        // If the NPCs cart gets hit hard by the player's cart, make them stunned.
-        if (col.gameObject.CompareTag("Cart") && col.gameObject.GetComponent<Rigidbody>().velocity.magnitude > stunMagnitude)
+        // If the NPCs cart gets hit by the player's cart while the player is dashing, make them stunned.
+        if (col.gameObject.CompareTag("Cart") && GameObject.FindGameObjectWithTag("Player").GetComponent<CharacterMovement>().IsDashing)
         {
             NPC npc = GetComponentInParent<NPC>();
             if (npc.myState == NPC.State.Stunned)
                 return;
 
-            print("PLAYER: STUNNED A BITCH");
             npc.myState = NPC.State.Stunned;
         }
     }
