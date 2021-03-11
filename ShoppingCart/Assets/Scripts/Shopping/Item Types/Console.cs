a918f618d9bac8d3600d17b9559f30ecd6ca849f
smithc57973 <44327733+smithc57973@users.noreply.github.com>
2021-03-10 19:44:08 -0600

    Updated spawnpoints
    Spawnpoints have been recustomized to be reduced and placed in the center of the aisles. The player interaction trigger has been moved forward to better pick items up. A new item type was added.

---
+++ ShoppingCart/Assets/Scripts/Shopping/Item Types/Console.cs
@@ -0,0 +1,16 @@
+/*****************************************************************************
+// File Name :         Console.cs
+// Author :            Kyle Grenier
+// Creation Date :     #CREATIONDATE#
+//
+// Brief Description : ADD BRIEF DESCRIPTION OF THE FILE HERE
+*****************************************************************************/
+using UnityEngine;
+
+public class Console : Item
+{
+    protected override string GetDescription()
+    {
+        return "A game console";
+    }
+}
