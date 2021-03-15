60c0c06add097b19ddc50b1a20ef152144c538c7
Kyle Grenier <kyle.grenier11@gmail.com>
2021-03-14 21:12:48 -0500

    Revised ItemFactory script to cater towards new way of spawning in loose items

--- ShoppingCart/Assets/Scripts/ItemFactory.cs
+++ ShoppingCart/Assets/Scripts/ItemFactory.cs
@@ -1,21 +1,36 @@
-using System.Collections;
-using System.Collections.Generic;
+/*****************************************************************************
+// File Name :         ItemFactory.cs
+// Author :            Kyle Grenier
+// Creation Date :     03/06/2021
+//
+// Brief Description : Manages retrieving an item prefab from the Resources folder.
+*****************************************************************************/
 using UnityEngine;
 
-public class ItemFactory : MonoBehaviour
+public static class ItemFactory
 {
-    public GameObject n;
-    public GameObject itemToSpawn;
-
-    public GameObject Spawn(string type)
+    /// <summary>
+    /// Returns a prefab of the item to spawn.
+    /// </summary>
+    /// <param name="itemName">The exact name of the item prefab in the Resources folder.</param>
+    /// <returns>A prefab of the item to spawn.</returns>
+    public static GameObject Spawn(string itemName)
     {
-        itemToSpawn = null;
+        // Retrieving the item prefab.
+        GameObject prefab = Resources.Load(itemName) as GameObject;
 
-        if (type.Equals("null"))
+        if (prefab == null)
         {
-            itemToSpawn = n;
+            Debug.LogWarning("ItemFactory: Cannot find a prefab named '" + itemName + "' in the Resources folder!");
+            return null;
         }
 
-        return itemToSpawn;
+        // Checking to see if it has the ItemContainer component. If it does not,
+        // add the component to it.
+        ItemContainer container = prefab.GetComponent<ItemContainer>();
+        if (container == null)
+            prefab.AddComponent<ItemContainer>();
+
+        return prefab;
     }
 }
--- ShoppingCart/Assets/Scripts/ItemSpawner.cs
+++ ShoppingCart/Assets/Scripts/ItemSpawner.cs
@@ -4,26 +4,26 @@ using UnityEngine;
 
 public class ItemSpawner : MonoBehaviour
 {
-    public ItemFactory factory;
-    public GameObject item;
-    public GameObject[] spawnPoints;
-    public int index;
+    //public ItemFactory factory;
+    //public GameObject item;
+    //public GameObject[] spawnPoints;
+    //public int index;
 
-    private void Start()
-    {
-        index = 0;
-        spawnPoints = GameObject.FindGameObjectsWithTag("Spawn");
-        factory = GameObject.FindGameObjectWithTag("Factory").GetComponent<ItemFactory>();
-        foreach (GameObject s in spawnPoints)
-        {
-            RequestSpawn("null");
-        }
-    }
+    //private void Start()
+    //{
+    //    index = 0;
+    //    spawnPoints = GameObject.FindGameObjectsWithTag("Spawn");
+    //    factory = GameObject.FindGameObjectWithTag("Factory").GetComponent<ItemFactory>();
+    //    foreach (GameObject s in spawnPoints)
+    //    {
+    //        RequestSpawn("null");
+    //    }
+    //}
 
-    public void RequestSpawn(string type)
-    {
-        item = factory.Spawn(type);
-        Instantiate(item, spawnPoints[index].transform.position, Quaternion.identity);
-        index++;
-    }
+    //public void RequestSpawn(string type)
+    //{
+    //    item = factory.Spawn(type);
+    //    Instantiate(item, spawnPoints[index].transform.position, Quaternion.identity);
+    //    index++;
+    //}
 }
